1e6a55e7ace6a7fc8d0c3ad6f7b18cfe14cc9fc9
Tristin Bui <tristinb@uci.edu>
2021-10-23 03:50:59 -0700

    movie list queries

--- src/MoviesServlet.java
+++ src/MoviesServlet.java
@@ -65,29 +65,38 @@ public class MoviesServlet extends HttpServlet {
             Statement statement2 = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             Statement statement3 = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
-            String query =  "SELECT DISTINCT movies.*, s.name as star, s.id as sId, g.name as genre, ratings.rating\n" +
-                            "FROM movies, \n" +
-                            "ratings,\n" +
-                            "(SELECT stars.*, sim.movieId as smId\n" +
-                            "FROM stars, stars_in_movies as sim\n" +
-                            "WHERE stars.id = sim.starId) as s,\n" +
-                            "(SELECT genres.*, gim.movieId as gmId\n" +
-                            "FROM genres, genres_in_movies as gim\n" +
-                            "WHERE genres.id = gim.genreId) as g\n" +
-                            "WHERE ratings.movieId = movies.id AND smID = movies.id AND gmId = movies.id";
+            String query1 = "SELECT movies.*, ratings.rating FROM movies, ratings WHERE ratings.movieId = movies.id";
+            String query2 = "SELECT stars.*, sim.movieId as smId FROM stars, stars_in_movies as sim WHERE stars.id = sim.starId";
+            String query3 = "SELECT genres.*, gim.movieId as gmId FROM genres, genres_in_movies as gim WHERE genres.id = gim.genreId";
 
             if(genre != null && !genre.isEmpty()){
-                query += String.format(" AND g.name = '%s' ORDER BY rating DESC ", genre);
+                query1 =    "SELECT movies.*, ratings.rating " +
+                            "FROM movies, ratings, genres_in_movies as gim, genres " +
+                            "WHERE genres.name = " +
+                            String.format("'%%%s%%' ", genre) +
+                            "AND genres.id = gim.genreId AND gim.movieId = movies.id AND ratings.movieId = movies.id";
+
+                query2 =    "SELECT stars.*, sim.movieId as smId FROM " +
+                            "stars, stars_in_movies as sim, genres_in_movies as gim, genres " +
+                            "WHERE genres.name = " +
+                            String.format("'%%%s%%' ", genre) +
+                            "AND genres.id = gim.genreId AND gim.movieId = movies.id AND stars.id = sim.starId";
+
+                query3 =    "SELECT genres.*, gim.movieId as gmId " +
+                            "FROM genres, genres_in_movies as gim " +
+                            "WHERE genrs.name = " +
+                            String.format("'%%%s%%' ", genre) +
+                            "AND genres.id = gim.genreId";
             }
 
             else if (index != null && !index.isEmpty()) { // browse by index
                 if (index.equals("*")){
                     // REGEX PATTERN FROM:
                     // https://stackoverflow.com/questions/1051583/fetch-rows-where-first-character-is-not-alphanumeric
-                    query += " AND movies.title REGEXP '^[^0-9A-Za-z]' ORDER BY rating DESC";
+                    query1 += " AND movies.title REGEXP '^[^0-9A-Za-z]' ORDER BY rating DESC";
                 }
                 else{
-                    query += " AND movies.title LIKE " + String.format("'%s%%' ORDER BY rating DESC", index);
+                    query1 += " AND movies.title LIKE " + String.format("'%s%%' ORDER BY rating DESC", index);
                 }
             }
 
@@ -95,44 +104,41 @@ public class MoviesServlet extends HttpServlet {
 
                 if(title != null && !title.isEmpty())
                 {
-                    query += " AND ";
-                    query = queryGenerator(query, title, 1);
+                    query1 += " AND ";
+                    query1 = queryGenerator(query1, title, 1);
 
                 }
 
                 if (year != null && !year.isEmpty()){
 
-                    query += " AND ";
+                    query1 += " AND ";
 
-                    query = queryGenerator(query, year, 2);
+                    query1 = queryGenerator(query1, year, 2);
 
                 }
 
                 if (director != null && !director.isEmpty()){
 
-                    query += " AND ";
+                    query1 += " AND ";
 
-                    query = queryGenerator(query, director, 3);
+                    query1 = queryGenerator(query1, director, 3);
 
                 }
 
                 if (star != null && !star.isEmpty()){
 
-                    query += " AND ";
+                    query1 += " AND ";
 
-                    query = queryGenerator(query, star, 4);
+                    query1 = queryGenerator(query1, star, 4);
                 }
             }
 
-            query = "SELECT movies.*, ratings.rating FROM movies, ratings WHERE ratings.movieId = movies.id ORDER BY movies.id";
-            String query2 = "SELECT stars.*, sim.movieId as smId FROM stars, stars_in_movies as sim WHERE stars.id = sim.starId ORDER BY smId";
-            String query3 = "SELECT genres.*, gim.movieId as gmId FROM genres, genres_in_movies as gim WHERE genres.id = gim.genreId ORDER BY gmId, genres.name ASC";
-
-//            query += String.format("LIMIT %s", mvct);
-
+            query1 += " ORDER BY movies.id";
+            query2 += " ORDER BY smId";
+            query3 += " ORDER BY gmId, genres.name ASC";
 
             // Perform the query
-            ResultSet rs_movie = statement1.executeQuery(query);
+            ResultSet rs_movie = statement1.executeQuery(query1);
             ResultSet rs_stars = statement2.executeQuery(query2);
             ResultSet rs_genres = statement3.executeQuery(query3);
 
